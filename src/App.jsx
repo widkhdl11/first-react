265628c76c547843851b94baa118d11995718273
widkhdl11 <wjdrudgml17@naver.com>
2023-09-14 17:22:15 +0900

    firstCommit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div className="flex justify-between max-w-screen-md max-auto bg-wight-100">
+    
+    <div className="">애플 아이콘</div>
+    
+    <div className="flex gap-8 hidden md:flex">
+
+      <div>메뉴</div>
+      <div>메뉴</div>
+      <div>메뉴</div>
+      <div>메뉴</div>
+      <div>메뉴</div>
+      <div>메뉴</div>
+    </div>
+    
+    <div>애플 아이콘</div>
+    </div>
+  );
+}
+
+export default App;
